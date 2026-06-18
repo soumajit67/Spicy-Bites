ec0f511652ce8766b2f5a6d656de0eee9f664c8d
Soumajit Chakraborty <156880318+soumajit67@users.noreply.github.com>
2026-06-18 10:00:07 +0530

    Update cart.js

--- cart.js
+++ cart.js
@@ -56,6 +56,9 @@ function populateCart() {
 
         cartProductsContainer.appendChild(cartItem);
     });
+         
+        const discount = 0;
+        const total = subtotal - discount;
 
 }
 
